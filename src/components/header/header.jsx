c6f5cdb58b2f11b7bf30f8d79f16b8f644036a1b
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-13 03:09:09 +0330

    header component created

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,8 +3,8 @@ import {TopNavigation} from "./top-navigation.jsx";
 
 export const Header = () => {
     return (
-        <header className='border-b border-base-content border-opacity-5'>
-            <div className='container flex items-center justify-between'>
+        <header>
+            <div className='container flex items-center justify-between w-[1440px] border-b border-base-content border-opacity-5'>
                 <img src={logo} height={64} width={64} alt='شاتل'/>
                 <TopNavigation/>
                 <span className='mr-auto'>Platform</span>
