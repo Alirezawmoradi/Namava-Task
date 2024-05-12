5ee8e5a0987170796523f319c8c055800bd516c4
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-12 22:15:15 +0330

    landing page structure created

---
+++ src/landing-page/landing-page.jsx
@@ -0,0 +1,7 @@
+export const LandingPage = () => {
+    return (
+        <div className='flex justify-center font-bold items-center text-3xl mt-10'>
+            <h1> Namava Landing Page</h1>
+        </div>
+    )
+}
