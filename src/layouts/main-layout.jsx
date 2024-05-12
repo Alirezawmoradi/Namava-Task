cb0fc1e4cc814ffa19449b765e348df309e845c8
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-12 23:32:33 +0330

    change layout styles

--- src/layouts/main-layout.jsx
+++ src/layouts/main-layout.jsx
@@ -1,17 +1,15 @@
 import {LandingPage} from "../landing-page/landing-page.jsx";
+import {Header} from "../components/header/header.jsx";
+import {Footer} from "../components/footer/footer.jsx";
 
 export const MainLayout = () => {
     return (
-        <div className='flex flex-col min-h-screen font-bold uppercase'>
-            <header className='bg-gray-200 flex items-center justify-center h-20'>
-                Header
-            </header>
+        <div className='min-h-screen grid grid-rows-[84px_1fr_auto] text-base-content'>
+            <Header/>
             <div className='flex-1 flex justify-center items-center'>
                 <LandingPage/>
             </div>
-            <footer className='bg-gray-200 flex items-center justify-center h-20'>
-                Footer
-            </footer>
+            <Footer/>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,13 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
+        colors: {
+            "orange": '#F36339',
+            "base-content": '#303539',
+        },
         extend: {
             backgroundImage: {
-                'landing-background':"url('/src/public/images/background-feedback-home-1 1.svg')"
+                'landing-background': "url('/src/public/images/background-feedback-home-1 1.svg')"
             }
         },
     },
