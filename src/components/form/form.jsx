d92dce0281d0cb846167394af47b5bc8d6ef9411
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-13 19:56:52 +0330

    form component created

---
+++ src/components/form/form.jsx
@@ -0,0 +1,32 @@
+import {Label} from "../label/label.jsx";
+import {TextBox} from "../textbox/textbox.jsx";
+import {Button} from "../button/button.jsx";
+
+export const Form = () => {
+    return (
+        <div
+            className='w-[676px] h-[598px] bg-base-100 mt-16 rounded-[20px] border border-base-content border-opacity-10 shadow-md mr-28'>
+            <form className='flex flex-col justify-center items-center gap-6 mt-16'>
+                <div className='flex flex-col'>
+                    <Label>نام و نام خانوادگی</Label>
+                    <TextBox/>
+                </div>
+                <div className='flex flex-col'>
+                    <Label>شماره تماس ثابت (به همراه کد شهر)</Label>
+                    <TextBox/>
+                </div>
+                <div className='flex flex-col'>
+                    <Label>شماره موبایل</Label>
+                    <TextBox/>
+                </div>
+                <div className='flex flex-col'>
+                    <Label>لطفا کاراکترها را تایپ کنید</Label>
+                    <TextBox/>
+                </div>
+                <div className='flex items-end ml-auto mr-8 mt-2'>
+                <Button variant='orange'>ارسال</Button>
+                </div>
+            </form>
+        </div>
+    )
+}
