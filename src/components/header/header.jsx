9b00635597e12509f2125cb9d3cacf0bf9f99995
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-13 13:30:20 +0330

    margin right changed

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,11 @@
-import logo from '/public/images/Logo shatel.svg';
+import logo from '/images/Logo shatel.svg';
 import {TopNavigation} from "./top-navigation.jsx";
 
 export const Header = () => {
     return (
         <header className='pt-4'>
             <div className='container flex items-center justify-between w-[1440px] bg-header border stroke-black border-opacity-5 drop-shadow'>
-                <img src={logo} height={64} width={64} alt='شاتل' className='mr-32'/>
+                <img src={logo} height={64} width={64} alt='شاتل' className='mr-40'/>
                 <TopNavigation/>
                 <div className='mr-auto'>
                 <span className='ml-32'>Platform</span>
