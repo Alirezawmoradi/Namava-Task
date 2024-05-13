89de388488fff7da7b940d042640e2c75297e39e
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-13 13:30:36 +0330

    text size changed

--- src/components/header/top-navigation.jsx
+++ src/components/header/top-navigation.jsx
@@ -19,7 +19,7 @@ export const TopNavigation = () => {
             {
                 menuItems.map((item) => {
                     return <li key={`navigation-${item.href}`}>
-                        <a href={item.href} className='hover:text-primary transition-colors pb-2'>
+                        <a href={item.href} className='text-sm hover:text-primary transition-colors pb-2'>
                             {item.title}
                         </a>
                     </li>
