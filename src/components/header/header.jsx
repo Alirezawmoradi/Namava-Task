44d60afe908e289d910cd0e86e0c15ba8a8316af
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-13 03:30:41 +0330

    header component refactored

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,7 +4,7 @@ import {TopNavigation} from "./top-navigation.jsx";
 export const Header = () => {
     return (
         <header>
-            <div className='container flex items-center justify-between w-[1440px] border border-base-content border-opacity-5'>
+            <div className='container flex items-center justify-between w-[1440px] bg-header border-base-content border-solid stroke-black border-opacity-5'>
                 <img src={logo} height={64} width={64} alt='شاتل' className='mr-32'/>
                 <TopNavigation/>
                 <div className='mr-auto'>
