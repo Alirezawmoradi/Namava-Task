778a035d7991bc8d0743a4405f2912625be310eb
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-13 04:28:26 +0330

    styles refactored

--- src/landing-page/landing-page.jsx
+++ src/landing-page/landing-page.jsx
@@ -1,7 +1,7 @@
 export const LandingPage = () => {
     return (
         <>
-            <section className='bg-landing-background h-[1019px] stroke-black drop-shadow bg-center bg-no-repeat -translate-y-6'>
+            <section className='bg-landing-background h-[1019px] stroke-black drop-shadow bg-center bg-no-repeat'>
 
             </section>
         </>
