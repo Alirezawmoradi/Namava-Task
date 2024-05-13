8e05f6eb68761a55ee7ca7470aa30c3a6d80ad89
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-13 19:49:05 +0330

    textbox component created

---
+++ src/components/textbox/textbox.jsx
@@ -0,0 +1,19 @@
+import classNames from "classnames";
+
+const sizeClasses = {
+    tiny: 'textbox-xs',
+    small: 'textbox-sm',
+    normal: 'textbox-md',
+    large: 'textbox-lg'
+}
+
+export const TextBox = ({variant = 'ghost', type = 'text', className, size = 'normal', ...rest}) => {
+    const classes = classNames(
+        'textbox',
+        'w-[612px]',
+        className,
+        {[`textbox-${variant}`]: variant},
+        {[`${sizeClasses[size]}`]: size}
+    )
+    return <input type={type} className={classes} {...rest}/>
+}
