353d0754c68737132847161405f16cdc566cbb74
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-14 04:48:17 +0330

    form component animated

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -5,7 +5,7 @@ import {Button} from "../button/button.jsx";
 export const Form = () => {
     return (
         <div
-            className='w-[676px] h-[598px] bg-base-100 mt-16 rounded-[20px] border border-base-content border-opacity-10 shadow-md mr-28'>
+            className='fade-in w-[676px] h-[598px] bg-base-100 mt-16 rounded-[20px] border border-base-content border-opacity-10 shadow-md mr-28'>
             <form className='flex flex-col justify-center items-center gap-6 mt-16'>
                 <div className='flex flex-col'>
                     <Label>نام و نام خانوادگی</Label>
