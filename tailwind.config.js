13beb127b53a099e3f9e600d83543d16e542bf14
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-14 04:49:59 +0330

    refactored

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,18 @@ export default {
         colors: {
             "orange": '#F36339',
             "base-content": '#303539',
-            "base-100":'#FFFFFF',
+            "base-100": '#FFFFFF',
             primary: "#2a68ff",
-            black:'#000000',
-            white:'#F5F5F5',
-            header:'#FEFEFE',
+            black: '#000000',
+            white: '#F5F5F5',
+            header: '#FEFEFE',
             transparent: "transparent",
-            ghost:'#D9D9D9',
+            ghost: '#D9D9D9',
             error: "#f87272",
-            required:'#FF0000'
+            required: '#FF0000',
+            current: "currentColor",
+            "gradient-first":'#F26322',
+            "gradient-second":'#F36339',
         },
         container: {
             center: true
@@ -24,7 +27,8 @@ export default {
         extend: {
             backgroundImage: {
                 'landing-background': "url('/images/background-feedback-home-1 1.svg')",
-                'movie-section':"url('/images/pic2.png')"
+                'movie-section': "url('/images/pic2.png')",
+                'icon-bg':"url('/images/Ellipse 1.svg')"
             }
         },
     },
