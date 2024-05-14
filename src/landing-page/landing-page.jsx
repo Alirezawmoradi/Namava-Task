5270d5c6c1dc0df8eb2dc74ceab94e790792087a
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-14 12:31:34 +0330

    refactored

--- src/landing-page/landing-page.jsx
+++ src/landing-page/landing-page.jsx
@@ -1,13 +1,14 @@
 import title from '/images/title line.png'
-import wifi from '/images/wifiicon.png'
-import cloud from '/images/cloud.png'
+
 import {Form} from "../components/form/form.jsx";
+import {MovieSection} from "../components/movie-section/movie-section.jsx";
 
 export const LandingPage = () => {
     return (
         <>
-            <section className='bg-landing-background h-[1019px] stroke-black drop-shadow-sm bg-center bg-no-repeat'>
-                <div className='flex flex-col container px-56 pt-20'>
+            <section
+                className='bg-landing-background container h-[1019px] stroke-black drop-shadow-sm bg-center bg-no-repeat'>
+                <div className='flex flex-col container px-44 pt-20'>
                     <h3 className='font-extrabold'>دریافت سرویس</h3>
                     <img src={title} height={10.34} width={100} alt='title'/>
                     <p className='w-1/2 mt-5 font-normal text-base leading-6'>برای بهره‌مندی از این شرایط و دریافت
@@ -15,14 +16,9 @@ export const LandingPage = () => {
                         کنید تا
                         همکاران ما در سریع‌ترین هنگام با شما تماس بگیرند.</p>
                 </div>
-                <div className='flex justify-between px-44'>
+                <div className='flex px-44'>
                     <Form/>
-                    <section className='bg-movie-section mt-16 bg-center bg-no-repeat flex flex-col justify-start'>
-                        <div className='flex items-center justify-center'>
-                            <img src={wifi} height={152.7} width={152.7} alt='wifi' className='mr-24'/>
-                            <img src={cloud} height={137} width={113} alt='cloud' className='mr-40 mb-12'/>
-                        </div>
-                    </section>
+                    <MovieSection/>
                 </div>
             </section>
         </>
