b6955936d571e5924e0c24556f49c00aa1edce39
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-16 00:45:34 +0330

    header responsive for md size

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,12 +5,13 @@ import {Button} from "../button/button.jsx";
 export const Header = () => {
     return (
         <header className='pt-4'>
-            <div className='container flex items-center justify-between w-full lg:w-[1440px] bg-header border stroke-black border-opacity-5 drop-shadow'>
-                <img src={logo} height={64} width={64} alt='شاتل' className='md:mr-32 mr-5'/>
+            <div
+                className='container flex items-center justify-between w-full lg:w-[1440px] bg-header border stroke-black border-opacity-5 drop-shadow'>
+                <img src={logo} height={64} width={64} alt='شاتل' className='lg:mr-32 md:ml-2 mr-5'/>
                 <TopNavigation/>
-                <div className='mr-auto ml-32 flex gap-x-8'>
-                    <Button variant='orange' className='lg:block hidden'>شاتل</Button>
-                    <Button className='lg:block hidden'>نماوا</Button>
+                <div className='mr-auto  ml-32 md:ml-2 flex gap-x-8 md:gap-x-6'>
+                    <Button variant='orange' size=''>شاتل</Button>
+                    <Button size=''>نماوا</Button>
                 </div>
             </div>
         </header>
