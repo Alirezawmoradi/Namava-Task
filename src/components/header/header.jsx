39485ec7e14d6f3b238f0cf9ca3c7f0a6dc83577
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-15 20:30:17 +0330

    header responsive

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,12 +5,12 @@ import {Button} from "../button/button.jsx";
 export const Header = () => {
     return (
         <header className='pt-4'>
-            <div className='container flex items-center justify-between w-[1440px] bg-header border stroke-black border-opacity-5 drop-shadow'>
-                <img src={logo} height={64} width={64} alt='شاتل' className='mr-32'/>
+            <div className='container flex items-center justify-between w-full lg:w-[1440px] bg-header border stroke-black border-opacity-5 drop-shadow'>
+                <img src={logo} height={64} width={64} alt='شاتل' className='md:mr-32 mr-5'/>
                 <TopNavigation/>
                 <div className='mr-auto ml-32 flex gap-x-8'>
-                    <Button variant='orange'>شاتل</Button>
-                    <Button>نماوا</Button>
+                    <Button variant='orange' className='lg:block hidden'>شاتل</Button>
+                    <Button className='lg:block hidden'>نماوا</Button>
                 </div>
             </div>
         </header>
