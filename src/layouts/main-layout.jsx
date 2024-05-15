d58430c2ad4090b957769a2b426c8ef7e2fc7882
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-15 20:31:02 +0330

    main layout responsive

--- src/layouts/main-layout.jsx
+++ src/layouts/main-layout.jsx
@@ -4,7 +4,7 @@ import {Footer} from "../components/footer/footer.jsx";
 
 export const MainLayout = () => {
     return (
-        <div className='min-h-screen grid grid-rows-[84px_1fr_auto] text-base-content bg-white' dir='rtl'>
+        <div className='min-h-screen min-w-80 md:grid md:grid-rows-[84px_1fr_auto] text-base-content bg-white' dir='rtl'>
             <Header/>
             <main>
                 <LandingPage/>
