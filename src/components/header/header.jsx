17dc3d9b65a197e2ad7cd9456c034b260c6a4a3a
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-16 01:24:24 +0330

    header responsive for sm size

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,9 +7,9 @@ export const Header = () => {
         <header className='pt-4'>
             <div
                 className='container flex items-center justify-between w-full lg:w-[1440px] bg-header border stroke-black border-opacity-5 drop-shadow'>
-                <img src={logo} height={64} width={64} alt='شاتل' className='lg:mr-32 md:ml-2 mr-5'/>
+                <img src={logo} height={64} width={64} alt='شاتل' className='lg:mr-32 md:ml-2 sm:ml-2 mr-5'/>
                 <TopNavigation/>
-                <div className='mr-auto  ml-32 md:ml-2 flex gap-x-8 md:gap-x-6'>
+                <div className='mr-auto  ml-32 md:ml-2 sm:ml-2 flex gap-x-8 md:gap-x-6'>
                     <Button variant='orange' size=''>شاتل</Button>
                     <Button size=''>نماوا</Button>
                 </div>
