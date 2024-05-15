b2c860280140107ccf8046450ab84d568197b9ab
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-15 20:30:09 +0330

    form responsive

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -5,8 +5,8 @@ import {Button} from "../button/button.jsx";
 export const Form = () => {
     return (
         <div
-            className='fade-in container w-[676px] h-[598px] bg-base-100 mt-16 rounded-[20px] border border-base-content border-opacity-10 shadow-md'>
-            <form className='flex flex-col justify-center items-center gap-6 mt-16'>
+            className='fade-in container lg:w-[676px] md:w-full lg:h-[598px] w-80 h-96 bg-base-100 lg:mt-16 -mt-7 mb-10 rounded-[20px] border border-base-content border-opacity-10 shadow-md'>
+            <form className='flex flex-col justify-center items-center lg:gap-6 gap-2 lg:mt-16 mt-2 text-xs lg:text-base'>
                 <div className='flex flex-col'>
                     <Label>نام و نام خانوادگی</Label>
                     <TextBox/>
@@ -23,8 +23,8 @@ export const Form = () => {
                     <Label>لطفا کاراکترها را تایپ کنید</Label>
                     <TextBox/>
                 </div>
-                <div className='flex items-end ml-auto mr-8 mt-2'>
-                <Button variant='orange' type='submit'>ارسال</Button>
+                <div className='flex items-end ml-auto lg:mr-8 md:mr-20 mr-4 lg:mt-2'>
+                <Button variant='orange' type='submit' size=''>ارسال</Button>
                 </div>
             </form>
         </div>
