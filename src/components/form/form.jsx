9e9602f888f7b866718f0ca05200789bc1445283
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-16 01:24:16 +0330

    form responsive for sm size

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -5,7 +5,7 @@ import {Button} from "../button/button.jsx";
 export const Form = () => {
     return (
         <div
-            className='fade-in container lg:w-[676px] md:w-full lg:h-[598px] w-80 h-96 bg-base-100 lg:mt-16 -mt-7 md:-mt-20 mb-10 rounded-[20px] border border-base-content border-opacity-10 shadow-md'>
+            className='fade-in container lg:w-[676px] md:w-full sm:w-8/12 lg:h-[598px] w-80 h-96 bg-base-100 lg:mt-16 -mt-7 md:-mt-20 sm:-mt-20 mb-10 rounded-[20px] border border-base-content border-opacity-10 shadow-md'>
             <form className='flex flex-col justify-center items-center lg:gap-6 gap-2 lg:mt-16 mt-2 text-xs lg:text-base'>
                 <div className='flex flex-col'>
                     <Label>نام و نام خانوادگی</Label>
@@ -23,7 +23,7 @@ export const Form = () => {
                     <Label>لطفا کاراکترها را تایپ کنید</Label>
                     <TextBox/>
                 </div>
-                <div className='flex items-end ml-auto lg:mr-8 md:mr-20 mr-4 lg:mt-2'>
+                <div className='flex items-end ml-auto lg:mr-8 md:mr-20 sm:mr-16 mr-4 lg:mt-2'>
                 <Button variant='orange' type='submit' size=''>ارسال</Button>
                 </div>
             </form>
