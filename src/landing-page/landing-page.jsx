2fa9625cc6c904d93d8745b901139a7bb864fcdc
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-15 20:30:25 +0330

    landing page responsive

--- src/landing-page/landing-page.jsx
+++ src/landing-page/landing-page.jsx
@@ -7,16 +7,16 @@ export const LandingPage = () => {
     return (
         <>
             <section
-                className='bg-landing-background container h-[1019px] stroke-black drop-shadow-sm bg-center bg-no-repeat'>
-                <div className='flex flex-col container px-44 pt-20'>
-                    <h3 className='font-extrabold'>دریافت سرویس</h3>
+                className='bg-landing-background container lg:h-[1019px] min-w-full stroke-black drop-shadow-sm bg-center bg-no-repeat'>
+                <div className='flex flex-col container lg:text-right lg:items-start justify-center items-center lg:px-44 lg:pt-20 px-10 pt-10'>
+                    <h3 className='font-extrabold text-sm lg:text-base'>دریافت سرویس</h3>
                     <img src={title} height={10.34} width={100} alt='title'/>
-                    <p className='w-1/2 mt-5 font-normal text-base leading-6'>برای بهره‌مندی از این شرایط و دریافت
+                    <p className='lg:w-1/2 mt-5 font-normal lg:text-base text-xs text-center lg:text-right leading-6'>برای بهره‌مندی از این شرایط و دریافت
                         سرویس‌های مختلف اینترنت پرسرعت شاتل فرم زیر را تکمیل
                         کنید تا
                         همکاران ما در سریع‌ترین هنگام با شما تماس بگیرند.</p>
                 </div>
-                <div className='flex px-44'>
+                <div className='flex flex-col-reverse lg:flex-row lg:px-44'>
                     <Form/>
                     <MovieSection/>
                 </div>
